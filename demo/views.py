fceb72a3d109bbabe04b16dda0a1ea25994a3dcc
simonrosen42 <simonrosen42@gmail.com>
2020-02-27 11:13:48 +0200

    Added login/logout functionality and updated coveralls to ignore manage.py

---
+++ demo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def home(request):
+    # return HttpResponse("Hello you are at home")
+    return render(request, 'demo/home.html')
