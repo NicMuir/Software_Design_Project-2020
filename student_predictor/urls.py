e930fa7da9c3130e6ef705e8710f77dd3a2715e7
Nic <admin@Nics-MacBook-Pro.local>
2020-05-02 18:42:51 +0200

    Got links working

--- student_predictor/urls.py
+++ student_predictor/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'student_predictor'
 urlpatterns = [
-    path('', views.ShowAllStudentsView.as_view(), name='show_all_students'),
+    path('Showall', views.ShowAllStudentsView.as_view(), name='show_all_students'),
     path('<int:pk>/', views.ShowStudentView.as_view(), name='show_student'),
     path('predict', views.PredictStudentView.as_view(), name='predict_student'),
 ]
