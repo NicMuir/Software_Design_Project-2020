e6b8b3743fe1cfc5642a7a3433371cce2b1d4d30
Simon Rosen <simonrosen173@gmail.com>
2020-05-02 17:13:42 +0200

    Added predict student functionality
    Integrated Support Vector Classifier to predict final 'risk' outcome of student. Code integrated into page under 'students/predict'.

--- student_predictor/urls.py
+++ student_predictor/urls.py
@@ -6,4 +6,5 @@ app_name = 'student_predictor'
 urlpatterns = [
     path('', views.ShowAllStudentsView.as_view(), name='show_all_students'),
     path('<int:pk>/', views.ShowStudentView.as_view(), name='show_student'),
+    path('predict', views.PredictStudentView.as_view(), name='predict_student'),
 ]
--- student_predictor/views.py
+++ student_predictor/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, HttpResponse, Http404, get_object_or_404
 from .models import *
 from django.urls import reverse
 from django.views import generic
+from django.urls import resolve
+from .apps import StudentPredictorConfig
+import pandas as pd
 
 # Create your views here.
-
-
 # Class based views
 class ShowAllStudentsView(generic.ListView):
     template_name = 'student_predictor/show_all_students.html'
@@ -20,14 +21,24 @@ class ShowStudentView(generic.DetailView):
     template_name = "student_predictor/show_student.html"
 
 
-# Old way of doing this
-# def show_all_students(request):
-#     alphabetical_students = Student.objects.order_by('first_name')
-#     context = {'alphabetical_students': alphabetical_students}
-#     return render(request, 'student_predictor/show_all_students.html', context)
-#
-#
-# def show_student(request, student_no):
-#     # If student does not exist site will be taken to 404 page
-#     student = get_object_or_404(Student, student_no=student_no)
-#     return render(request, 'student_predictor/show_student.html', {'student': student})
+class PredictStudentView(generic.CreateView):
+    model = Student
+    fields = ['student_no','first_name', 'last_name', 'aggregate_YOS1', 'aggregate_YOS2',
+              'coms_avg_YOS1', 'coms_avg_YOS2', 'maths_avg_YOS1', 'maths_avg_YOS2']
+    template_name = "student_predictor/predict_student.html"
+
+    # If student model is created successfully run following code to add predicted data
+    def form_valid(self, form):
+        out_response = super().form_valid(form)
+        # Do stuff here
+        data_dict = self.object.predict_data()
+        stud_data = pd.DataFrame.from_dict(data_dict)
+
+        # TODO - Test
+        # prediction = self.svc_predictor.predict(df_data)
+
+        self.object.prediction = StudentPredictorConfig.svc_predictor.predict(stud_data)[0]
+
+        self.object.save()
+        return out_response
+
