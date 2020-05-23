ce1994dc3a36983ae75890f6a4494239fd380016
Simon Rosen <simonrosen173@gmail.com>
2020-05-23 09:26:11 +0200

    Added re-predict student functionality & added backend functionality to allow for user to be 'selected' when going into show all students via a variable passed through the url.

--- student_predictor/urls.py
+++ student_predictor/urls.py
@@ -5,7 +5,13 @@ from . import views
 
 app_name = 'student_predictor'
 urlpatterns = [
+    # <int:student_id>
     path('', login_required(views.ShowAllStudentsView.as_view()), name='show_all_students'),
+    # show all students with specific student to show
+    path('<int:student_pk>/', login_required(views.ShowAllStudentsView.as_view()), name='show_all_students_selected'),
+    # show_student needs to be removed but first, dependencies need to be fixed
     path('<int:pk>/', login_required(views.ShowStudentView.as_view()), name='show_student'),
+
     path('predict', login_required(views.PredictStudentView.as_view()), name='predict_student'),
+    path('predict/<int:pk>/', login_required(views.RePredictStudentView.as_view()), name='re_predict_student'),
 ]
--- student_predictor/views.py
+++ student_predictor/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse, Http404, get_object_or_404
 from .models import *
+from .forms import *
+
 from django.urls import reverse
 from django.views import generic
 from django.urls import resolve
 from .apps import StudentPredictorConfig
 import pandas as pd
+
 
 # Create your views here.
 # Class based views
@@ -14,7 +17,24 @@ class ShowAllStudentsView(generic.ListView):
     context_object_name = 'alphabetical_students'
 
     def get_queryset(self):
+        # print(self.kwargs["student_pk"])
         return Student.objects.order_by('first_name')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # Add new variables to context
+        context = super(ShowAllStudentsView, self).get_context_data(**kwargs)
+
+        if "student_pk" in self.kwargs.keys():
+            try:
+                context['selected_student'] = Student.objects.get(pk=self.kwargs["student_pk"])
+            except Student.DoesNotExist:
+                print("Student does not exist")
+                context['selected_student'] = None
+        else:
+            print("student_pk was not in kwargs")
+            context['selected_student'] = None
+
+        return context
 
 
 # @login_required
@@ -37,6 +57,42 @@ class PredictStudentView(generic.CreateView):
         data_dict = self.object.predict_data()
         stud_data = pd.DataFrame.from_dict(data_dict)
 
+        # prediction = self.svc_predictor.predict(df_data)
+
+        self.object.prediction = StudentPredictorConfig.svc_predictor.predict(stud_data)[0]
+
+        self.object.save()
+        return out_response
+
+    def get_context_data(self, **kwargs):
+        ctx = super(PredictStudentView, self).get_context_data(**kwargs)
+        # Below copy and pasted, need to read up on this
+        if self.request.POST:
+            ctx['file_upload_form'] = UploadFileForm(self.request.POST)  # Keeps data I think
+        else:
+            ctx['file_upload_form'] = UploadFileForm()
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        out = super().post(self, request, *args, **kwargs)
+        # # https://chriskief.com/2012/12/30/django-class-based-views-with-multiple-forms/
+        return out
+
+
+# @login_required
+class RePredictStudentView(generic.UpdateView):
+    model = Student
+    fields = ['student_no', 'first_name', 'last_name', 'aggregate_YOS1', 'aggregate_YOS2',
+              'coms_avg_YOS1', 'coms_avg_YOS2', 'maths_avg_YOS1', 'maths_avg_YOS2']
+    template_name = "student_predictor/re_predict_student.html"
+
+    # If student model is created successfully run following code to add predicted data
+    def form_valid(self, form):
+        out_response = super().form_valid(form)
+        # Do stuff here
+        data_dict = self.object.predict_data()
+        stud_data = pd.DataFrame.from_dict(data_dict)
+
         # TODO - Test
         # prediction = self.svc_predictor.predict(df_data)
 
@@ -44,4 +100,6 @@ class PredictStudentView(generic.CreateView):
 
         self.object.save()
         return out_response
+
+
 
