408f7df6bc390d4db766fb71b391ce7c92ea14da
Simon Rosen <simonrosen173@gmail.com>
2020-05-24 16:08:23 +0200

    Commented out broken code

--- tests/tests_views.py
+++ tests/tests_views.py
@@ -20,33 +20,33 @@ class TestShowAllStudentsView(TestCase):
         self.client.login(username=my_admin.username, password=password)
 
     # generic.ListView
-    def TestShowAllStudentsView(self):
-        login_as_superuser();
-        # Do stuff
-        return True
+    # def TestShowAllStudentsView(self):
+    #     self.login_as_superuser()
+    #     # Do stuff
+    #     return True
 
     # generic.CreateView
-    def TestPredictStudentView(self):
-        login_as_superuser();
-        predict_data_dict = dict(
-            AggregateYOS1=[60.4],
-            AggregateYOS2=[53.2],
-            FYComsAvg=[70.5],
-            SYComsAvg=[67.4],
-            FYMathAvg=[60.3],
-            SYMathAvg=[54.3],
-        )
-        self.assertDictEqual(self.temp_stud.predict_data(), predict_data_dict)
-        # Do stuff
-        return True
+    # def TestPredictStudentView(self):
+    #     self.login_as_superuser()
+    #     predict_data_dict = dict(
+    #         AggregateYOS1=[60.4],
+    #         AggregateYOS2=[53.2],
+    #         FYComsAvg=[70.5],
+    #         SYComsAvg=[67.4],
+    #         FYMathAvg=[60.3],
+    #         SYMathAvg=[54.3],
+    #     )
+    #     self.assertDictEqual(self.temp_stud.predict_data(), predict_data_dict)
+    #     # Do stuff
+    #     return True
 
-    def TestPredictMultiStudentView(self):
-        login_as_superuser();
-        # Do stuff
-        return True
-
-    # generic.UpdateView
-    def TestRePredictStudentView(self):
-        login_as_superuser();
-        # Do stuff
-        return True
+    # def TestPredictMultiStudentView(self):
+    #     login_as_superuser();
+    #     # Do stuff
+    #     return True
+    #
+    # # generic.UpdateView
+    # def TestRePredictStudentView(self):
+    #     login_as_superuser();
+    #     # Do stuff
+    #     return True
