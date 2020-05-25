60863816a617a5f3883b7ba25db4f663fc32b722
Simon Rosen <simonrosen173@gmail.com>
2020-05-25 13:10:05 +0200

    Moved header, navbar and footer html to seperate files that are just included in each file. Minor UI changes. Added description to Predict Multi Student Page

--- SD_Project/urls.py
+++ SD_Project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('demo/', include('demo.urls')),
+    path('home/', include('demo.urls')),
     path('students/', include('student_predictor.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
