089964348d6fcaf7f80e1b90468f15510bc0243c
Simon Rosen <simonrosen173@gmail.com>
2020-05-30 13:33:36 +0200

    Added tests in student_predictor/views.py

--- tests/tests_views.py
+++ tests/tests_views.py
@@ -10,7 +10,7 @@ import pandas as pd
 from student_predictor.models import Student
 
 
-class TestShowAllStudentsView(TestCase):
+class TestViews(TestCase):
     # This method is run before any other tests i.e. sets up whatever is needed for tests
     def setUp(self):
         self.login_as_superuser()  # Need to be logged in
@@ -29,12 +29,49 @@ class TestShowAllStudentsView(TestCase):
         PredictMultiStudentView.handle_file(correct_file)
         self.assertEquals(Student.objects.count(), prev_no_students+5)
 
+    # url kwarg with pk of student that exists
+    def test_SASV_student_exists_arg(self):
+        # create student
+        stud_vals = dict(
+            student_no=12345,
+            first_name="John",
+            last_name="Doe",
+            # ML Predict Data
+            aggregate_YOS1=60.4,
+            aggregate_YOS2=53.2,
+            coms_avg_YOS1=70.5,
+            coms_avg_YOS2=67.4,
+            maths_avg_YOS1=60.3,
+            maths_avg_YOS2=54.3,
+            prediction='H',
+        )
+        temp_student = Student.objects.create(**stud_vals)
+        student_pk = temp_student.pk
+
+        status_code = 200
+        view_class = ShowAllStudentsView
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        resp = ShowAllStudentsView.as_view()(req, *[], **{"student_pk": student_pk})
+        self.assertEqual(resp.status_code, 200)
+
+    # url kwarg with pk of student that does not exist
+    def test_SASV_student_does_not_exist_arg(self):
+        student_pk = 10000  # student pk that will definitely not exist
+
+        status_code = 200
+        view_class = ShowAllStudentsView
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        resp = ShowAllStudentsView.as_view()(req, *[], **{"student_pk": student_pk})
+        self.assertEqual(resp.status_code, 200)
+
     def test_SASVget(self):
         status_code = 200
         view_class = ShowAllStudentsView
         req = RequestFactory().get('/')
         req.user = AnonymousUser()
-        resp = ShowAllStudentsView.as_view() (req , *[],**{})
+        resp = ShowAllStudentsView.as_view()(req, *[], **{})
         self.assertEqual(resp.status_code,200)
 
     def test_PSVget(self):
