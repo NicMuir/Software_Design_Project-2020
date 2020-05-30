fe162810e05036fd9c1e9fc9456644d6366e3f19
Simon Rosen <simonrosen173@gmail.com>
2020-05-30 12:08:00 +0200

    Attempted fix for build issues

--- student_predictor/apps.py
+++ student_predictor/apps.py
@@ -9,12 +9,3 @@ class StudentPredictorConfig(AppConfig):
     # Load ML model
     filename = "student_predictor/svc_model.pickle"  # TODO
     svc_predictor = pickle.load(open(filename, 'rb'))
-
-    # TODO
-    # def predict_student(self, student):
-    #     data_dict = student.predict_data()
-    #     df_data = pd.DataFrame.from_dict(data_dict)
-    #
-    #     # TODO - Test
-    #     prediction = self.svc_predictor.predict(df_data)
-    #     return prediction
